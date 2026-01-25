f63efb940de80506013d9f51e3b4fc6a8ce141cb
Aditya <adityas.singh.tech@gmail.com>
2026-01-25 15:03:30 +0530

    style: change icon color for chatbot

--- components/Chatbot/Chatbot.tsx
+++ components/Chatbot/Chatbot.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { MdChat, MdClose } from "react-icons/md";
-import  ChatWindow  from "./ChatWindow";
+import ChatWindow from "./ChatWindow";
 
 const Chatbot = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -18,14 +18,14 @@ const Chatbot = () => {
             {/* Chatbot Button */}
             <button
                 onClick={toggleChat}
-                className={`flex h-14 w-14 items-center justify-center rounded-full bg-blue-600 text-white shadow-lg transition-all hover:bg-blue-700 active:scale-95 relative ${isOpen ? "rotate-90 scale-0 opacity-0" : "rotate-0 scale-100 opacity-100"
+                className={`flex h-14 w-14 items-center justify-center rounded-full bg-black text-white shadow-lg transition-all hover:bg-black/80 hover:cursor-pointer active:scale-95 relative ${isOpen ? "rotate-90 scale-0 opacity-0" : "rotate-0 scale-100 opacity-100"
                     }`}
             >
                 <MdChat size={28} />
                 {hasNewMessage && !isOpen && (
-                    <span className="absolute -top-1 -right-1 flex h-4 w-4">
-                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-4 w-4 bg-red-500"></span>
+                    <span className="absolute -top-1 -right-0 flex h-4 w-4">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-500 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-4 w-4 bg-blue-500"></span>
                     </span>
                 )}
             </button>
